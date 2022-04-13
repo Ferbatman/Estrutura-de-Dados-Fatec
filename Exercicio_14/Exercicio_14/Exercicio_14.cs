7c319b4aa67d6c0308e31bffa29243ce210632f7
Fernando Silvestre <fernandohsilvestre@hotmail.com>
2022-04-13 17:22:50 -0300

    Exercícios de fila

---
+++ Exercicio_14/Exercicio_14/Exercicio_14.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_14
+{
+    internal class Exercicio_14
+    {
+        const int MAX = 20;
+
+        static void Insere(int[] q, ref int f, int v)
+        {
+            q[f] = v;
+            f = f + 1;
+        }
+
+        static int Remove(int[] q, ref int i)
+        {
+            return (q[i++]);
+        }
+
+        static bool EstaVazia(int i, int f)
+        {
+            if (i == f)
+                return true;
+            else
+                return false;
+        }
+
+        static bool EstaCheia(int f)
+        {
+            if (f == MAX)
+                return true;
+            else
+                return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] fila = new int[MAX];
+            int inicio = 0, fim = 0, maior = -999, menor = 999; 
+            double soma = 0;
+
+            Console.WriteLine("Quantos numeros quer adicionar? ");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numero; i++)
+            {
+                Console.WriteLine("\nInsira um valor: ");
+                int n1 = int.Parse(Console.ReadLine());
+                Insere(fila, ref fim, n1);
+            }
+            
+            for (int i = inicio; i < fim; i++)
+            {
+                int n2 = Remove(fila, ref inicio);
+                if (maior < n2)
+                    maior = n2;
+                
+                if (menor > n2)
+                    menor = n2;
+                
+                soma += n2;
+            }
+            double media = soma / numero;
+            Console.WriteLine($"\nO maior número da fila é {maior}");
+            Console.WriteLine($"\nO menor número da fila é {menor}");
+            Console.WriteLine($"\nA média dos número da fila é {media:F2}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio_15/Exercicio_15/Exercicio_15.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_15
+{
+    internal class Exercicio_15
+    {
+        const int MAX = 20;
+
+        static void Insere(int[] q, ref int f, int v)
+        {
+            q[f] = v;
+            f = f + 1;
+        }
+
+        static int Remove(int[] q, ref int i)
+        {
+            return (q[i++]);
+        }
+
+        static bool EstaVazia(int i, int f)
+        {
+            if (i == f)
+                return true;
+            else
+                return false;
+        }
+
+        static bool EstaCheia(int f)
+        {
+            if (f == MAX)
+                return true;
+            else
+                return false;
+        }
+
+        static int QtdAvioes(int f, int i)
+        {
+            return f - i;
+        }
+
+        static void ListaAvioes(int[] q, int i,int f)
+        {
+            int j;
+            for (j = i; j < f; j++)
+            Console.WriteLine(q[j]);
+        }
+
+        static int PrimeiraDaFila(int[] q, int i)
+        {
+            return q[i];
+        }
+
+        static void Main(string[] args)
+        {
+            int[] fila = new int[MAX];
+            int inicio = 0, fim = 0;
+            string op = "0";
+
+            while (op != "6")
+            {
+                Console.WriteLine("\nMENU");
+                Console.WriteLine("1 - Adiciona na fila");
+                Console.WriteLine("2 - Consulta a quantidade de aviões");
+                Console.WriteLine("3 - Autoriza a decolagem");
+                Console.WriteLine("4 - Lista os aviões");
+                Console.WriteLine("5 - Consulta o primeiro da fila");
+                Console.WriteLine("6 - Sair");
+                Console.WriteLine("\nDigite a opção desejada:");
+                op = Console.ReadLine();
+
+                if (op == "1")
+                {
+                    Console.WriteLine("\nNumero do avião: ");
+                    int aviao;
+                    aviao = int.Parse(Console.ReadLine());
+                    if (EstaCheia(fim) == false)
+                        Insere(fila, ref fim, aviao);
+                    else
+                        Console.WriteLine("Fila Cheia");
+                }
+
+                else if (op == "2")
+                {
+                    int qtd = QtdAvioes(fim, inicio);
+                    Console.WriteLine("A quantidade de aviões é " + qtd);
+                }
+
+                else if (op == "3")
+                {
+                    int aviao;
+                    aviao = Remove(fila, ref inicio);
+                    Console.WriteLine("\nAvião que decolou " + aviao);
+                }
+
+                else if (op == "4")
+                {
+                    ListaAvioes(fila, inicio, fim);
+                }
+
+                else if (op == "5")
+                {
+                    Console.WriteLine(PrimeiraDaFila(fila, inicio));
+                }
+            }
+
+        }
+    }
+}
